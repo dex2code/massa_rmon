78a20016a11d88b5e7777db013ec2ee7b76fa1d8
dex2code <dex2code@localhost>
2024-01-14 00:13:45 +0300

    commit

---
+++ install-service.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+echo -n "Generating service file... "
+echo "
+[Unit]
+Description=MASSA Remote Monitoring service
+Wants=network.target
+After=network.target
+
+[Service]
+Type=idle
+User=$USER
+WorkingDirectory=$HOME/massa_rmon
+ExecStart=$HOME/massa_rmon/bin/python3 main.py
+Restart=always
+RestartSec=60
+
+[Install]
+WantedBy=multi-user.target
+" > ./massa_rmon.service
+
+if [[ $? -eq 0 ]]
+then
+        echo "Done!"
+else
+        echo "Error!"
+        exit 1
+fi
+
+
+echo "Copying service file to /etc/systemd/system/massa_rmon.service... "
+sudo cp ./massa_rmon.service /etc/systemd/system/massa_rmon.service
+
+if [[ $? -eq 0 ]]
+then
+        echo "Done!"
+else
+        echo "Error!"
+        exit 1
+fi
+
+
+echo "Reloading systemd daemon configuration... "
+sudo systemctl daemon-reload
+
+if [[ $? -eq 0 ]]
+then
+        echo "Done!"
+else
+        echo "Error!"
+        exit 1
+fi
+
+
+echo
+echo "Success!"
